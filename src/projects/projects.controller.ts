5623b2996de49ae28c036a3348914438ef67e8b7
hederalves <alvesheder92@gmail>
2025-03-31 20:52:57 -0300

    chore: remove duplicate files

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param } from "@nestjs/common"
-import { Projeto } from "@core"
+import { Project } from "@core"
 import { ProjectPrisma } from "./projects.prisma"
 
 @Controller("projects")
@@ -7,12 +7,12 @@ export class ProjectController {
     constructor(private readonly repo: ProjectPrisma) { }
 
     @Get()
-    async obterTodos(): Promise<Projeto[]> {
+    async obterTodos(): Promise<Project[]> {
         return this.repo.obterTodos()
     }
 
     @Get(":id")
-    async obterPorId(@Param("id") id: string): Promise<Projeto | null> {
+    async obterPorId(@Param("id") id: string): Promise<Project | null> {
         return this.repo.obterPorId(Number(id))
     }
 }
--- src/projects/projects.prisma.ts
+++ src/projects/projects.prisma.ts
@@ -1,4 +1,4 @@
-import { Projeto } from "@core"
+import { Project } from "@core"
 import { Injectable } from "@nestjs/common"
 import { PrismaProvider } from "src/db/prisma.provider"
 
@@ -6,11 +6,11 @@ import { PrismaProvider } from "src/db/prisma.provider"
 export class ProjectPrisma {
     constructor(private readonly prisma: PrismaProvider) { }
 
-    async obterTodos(): Promise<Projeto[]> {
+    async obterTodos(): Promise<Project[]> {
         return this.prisma.project.findMany() as any
     }
 
-    async obterPorId(id: number): Promise<Projeto | null> {
+    async obterPorId(id: number): Promise<Project | null> {
         return this.prisma.project.findUnique({
             where: { id },
             include: { technologies: true },
